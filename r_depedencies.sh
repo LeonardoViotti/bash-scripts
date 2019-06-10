0bb5e46f1f474c98371040e9022cf31f8a072918
LeonardoViotti <leonardoviotti@gmail.com>
2019-06-10 09:29:50 -0400

    Added r_dependencies.sh script

---
+++ r_depedencies.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# This script install r packages dependencies
+
+# Tidyverse
+sudo apt-get install g++
+sudo apt install libcurl4-openssl-dev libssl-dev libxml2-dev
+
+# Spatial packages
+sudo apt-get install libgdal-dev
+sudo apt-get install libgeos-dev
+
+# sf, lfe and readstata13
+sudo apt-get install gfortran
+sudo apt-get install r-base-dev
+sudo apt-get install libudunits2-dev 
