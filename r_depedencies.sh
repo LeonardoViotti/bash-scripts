bc49be050857d55a884a315ce0e6d031d9f547a3
leonardoviotti <leonardoviotti@gmail.com>
2020-01-14 14:29:40 -0300

    Added some R packages installation dependencies.

--- r_depedencies.sh
+++ r_depedencies.sh
@@ -14,3 +14,7 @@ sudo apt-get install libgeos-dev
 sudo apt-get install gfortran
 sudo apt-get install r-base-dev
 sudo apt-get install libudunits2-dev 
+
+# For Rfast
+sudo apt-get install libgsl0-dev
+
