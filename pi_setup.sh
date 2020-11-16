9540a707f108a7a0d4c8f07d1140ec33653bf1a9
LeonardoViotti <leonardoviotti@gmail.com>
2020-11-16 16:14:48 -0500

    Draft raspberrypi set-up.

---
+++ pi_setup.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# General updates
+sudo apt update
+sudo apt upgrade
+
+# Install vim
+sudo apt install vim
+
+# Install VSC
+
+# Install docker
+curl -fsSL https://get.docker.com -o get-docker.sh
+sudo sh get-docker.sh
