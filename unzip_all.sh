4a802a2430b97aaaede5e8475e906ad5ca67b536
LeonardoViotti <leonardoviotti@gmail.com>
2020-12-04 17:39:08 -0500

    Improved unzip_all to include .rar

--- unzip_all.sh
+++ unzip_all.sh
@@ -2,7 +2,17 @@
 
 # https://stackoverflow.com/a/29920087/8692138
 
+# Uncompress .zip
 for i in *.zip; do
   newdir="${i:0:-4}" && mkdir "$newdir"
   unzip "$i" -d  "$newdir"
 done
+
+# Uncompress .rar
+for i in *.rar; do
+  unar "$i" -d  "$newdir"
+done
+
+
+
+
