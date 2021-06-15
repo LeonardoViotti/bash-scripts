c89e00557160f8908edd824a457b9b23c60dc11b
LeonardoViotti <leonardoviotti@gmail.com>
2021-06-15 08:33:17 -0400

    Update mount-external-hd.sh

--- raspberry-pi/mount-external-hd.sh
+++ raspberry-pi/mount-external-hd.sh
@@ -1,3 +1,5 @@
+# https://www.raspberrypi.org/documentation/configuration/external-storage.md
+
 # List partitions with a few extra details
 sudo lsblk -o UUID,NAME,FSTYPE,SIZE,MOUNTPOINT,LABEL,MODEL
 
