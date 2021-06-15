b4d8e8052b21e5465d23f2b263be4f00eb2aab6d
LeonardoViotti <leonardoviotti@gmail.com>
2021-06-15 08:30:15 -0400

    Create mount-external-hd.sh

---
+++ raspberry-pi/mount-external-hd.sh
@@ -0,0 +1,9 @@
+# List partitions with a few extra details
+sudo lsblk -o UUID,NAME,FSTYPE,SIZE,MOUNTPOINT,LABEL,MODEL
+
+# Get the exact location of partitions
+sudo blkid
+
+# Mount a partition to a new folder
+ sudo mkdir /media/pi/LTVNTFS
+ sudo mount /dev/sda2 /media/pi/LTVNTFS
