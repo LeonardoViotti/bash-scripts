937bee3b18e3b902a5c8dce79bede3268aa1a9fd
LeonardoViotti <leonardoviotti@gmail.com>
2022-07-29 15:05:12 -0400

    NordVPN installation on setup.sh

--- setup.sh
+++ setup.sh
@@ -2,6 +2,16 @@
 
 cd ~
 
+sudo apt update
+sudo apt updgrade
+
+#---------------------------
+# Utils
+
+sudo apt install curl
+sudo apt install gcc
+
+
 #---------------------------
 # QGIS
 
@@ -18,7 +28,7 @@ sudo apt install qgis qgis-plugin-grass
 # CUDA
 
 # Some dependencies and utilities
-sudo apt update && sudo apt install gcc
+# sudo apt update && sudo apt install gcc
 sudo apt update && sudo apt install build-essential
 sudo apt update && sudo apt install libglvnd-dev pkg-config
 sudo apt update && sudo apt install freeglut3 freeglut3-dev libxi-dev libxmu-dev\
@@ -32,3 +42,24 @@ sudo dpkg -i cuda-repo-ubuntu2204-11-7-local_11.7.0-515.43.04-1_amd64.deb
 sudo cp /var/cuda-repo-ubuntu2204-11-7-local/cuda-*-keyring.gpg /usr/share/keyrings/
 sudo apt-get update
 sudo apt-get -y install cuda
+
+#---------------------------
+# NordVPN
+
+wget https://repo.nordvpn.com/deb/nordvpn/debian/pool/main/nordvpn-release_1.0.0_all.deb
+sudo apt-get install nordvpn-release_1.0.0_all.deb
+sudo apt-get update
+sudo apt-get install nordvpn
+
+
+
+# Make into a function?
+# echo "Press any key to continue"
+# while [ true ] ; do
+#     read -t 3 -n 1
+#     if [ $? = 0 ] ; then
+#     exit ;
+# else
+#     echo "waiting for the keypress"
+# fi
+# done
