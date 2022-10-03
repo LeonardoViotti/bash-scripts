7d42b44122586e19e4c44c6c36ac5b9f1a1ab529
LeonardoViotti <leonardoviotti@gmail.com>
2022-10-03 09:51:12 -0400

    Conda envs minor updates.

--- conda-envs/gis.sh
+++ conda-envs/gis.sh
@@ -3,13 +3,12 @@
 # GIS conda environment
 
 
-conda create -n gis python=3.8
+conda create -n gis python=3.8 -y
 conda activate gis
 
 pip install jupyterlab
 pip install earthengine-api
 pip install geemap
-
-conda install --channel conda-forge geopandas
-# conda install -c conda-forge folium
+pip install geopandas
+# pip install folium
 
--- conda-envs/torch.sh
+++ conda-envs/torch.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
 # Create env
-conda create -n torch-p3.8 python=3.8
+conda create -n torch-p3.8 python=3.8 -y
+
 conda activate torch-p3.8
 
 # Pytorch
-conda install pytorch torchvision torchaudio cudatoolkit=11.6 -c pytorch -c conda-forge
+pip3 install torch torchvision torchaudio --extra-index-url https://download.pytorch.org/whl/cu116
 
 # OpenCV
 sudo apt install libopencv-dev python3-opencv
@@ -13,5 +14,4 @@ pip install opencv-python
 
 # Other libraries
 pip install opensoundscape==0.7.0
-conda install pandas
 pip install boto3
