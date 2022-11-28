d7f2ff03b76876a0d79b765e3deb1e9790af786c
LeonardoViotti <leonardoviotti@gmail.com>
2022-11-28 10:33:11 +0000

    Turn off mid buttom click on touch pad (hopefully this is working.

---
+++ utils/turn-off-mid-buttom-touchpad.sh
@@ -0,0 +1,13 @@
+
+# https://askubuntu.com/questions/597064/disabling-middle-mouse-button
+# https://wiki.ubuntu.com/X/Config/Input
+
+# Check input id
+xinput list
+
+# Disable middle buttom
+# xinput set-button-map 18 1 0 3
+
+# Turn the middle-mouse button into a left-mouse button by setting it to 1:
+# xinput set-button-map 18 1 1 3
+
