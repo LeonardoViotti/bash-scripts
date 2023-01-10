2d054b099d28130ae134095885157eb01ff15c6a
LeonardoViotti <leonardoviotti@gmail.com>
2023-01-10 12:00:20 -0300

    Draft gray hat conda env.

--- conda-envs/ghat.sh
+++ conda-envs/ghat.sh
@@ -2,4 +2,12 @@
 
 # Gray hat-  Webscraping and security conda environment
 
+eval "$(conda shell.bash hook)"
+# conda create -n ghat python=3.11 -y
+conda init bash
+conda create -n ghat python=3.10 -y
+conda activate ghat
+
 pip install youtube_dl
+pip install pytube
+pip install selenium
